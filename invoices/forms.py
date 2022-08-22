55686115492123ac57a3111345f4cd95ab758803
icanbe444 <icanbe444@gmail.com>
2022-08-22 15:04:17 +0100

    I changed the success_url to simple_template.html

--- invoices/forms.py
+++ invoices/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Invoice
 
 
 
 class InvoiceForm(forms.ModelForm):
-    completion_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    completion_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), label='Real Completion Date')
     issue_date      = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     payment_date    = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     
@@ -17,3 +18,14 @@ class InvoiceForm(forms.ModelForm):
             'issue_date', 
             'payment_date'
             )
+        labels = {#dates cannot be changed here
+                'receiver': 'Customer',
+                
+
+            }
+
+    def clean_number(self):
+        number = self.cleaned_data.get('number')
+        if len(number) < 10:
+            raise ValidationError('Number is too short')
+        return number
--- invoices/urls.py
+++ invoices/urls.py
@@ -1,11 +1,13 @@
 from .views import (
     InvoiceListView,
-    InvoiceFormView
+    InvoiceFormView,
+    SimpleTemplateView
 )
 from django.urls import path
 
 app_name = 'invoices'
 urlpatterns = [
     path('', InvoiceListView.as_view(), name = 'main'),
-    path('new/', InvoiceFormView.as_view(), name = 'create')
+    path('new/', InvoiceFormView.as_view(), name = 'create'),
+    path('<pk>/', SimpleTemplateView.as_view(), name = 'simple-template'),
 ]
