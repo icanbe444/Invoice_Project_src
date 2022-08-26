af0698894fba5ed4e7c03fb90e18aed5bd812ab0
icanbe444 <icanbe444@gmail.com>
2022-08-26 21:24:40 +0100

    Changed my amount to decimal field

--- positions/models.py
+++ positions/models.py
@@ -7,10 +7,12 @@ class Positions(models.Model):
     invoice     = models.ForeignKey(Invoice, on_delete=models.CASCADE)
     title       = models.CharField(max_length=200)
     description = models.TextField(blank=True, help_text= "Optional Info")
-    amount      = models.FloatField(help_text="in US $")
+    amount      = models.DecimalField(help_text="in Nigerian N", max_digits=15, decimal_places=2)
     created     = models.DateTimeField(auto_now_add=True)
     
 
     def __str__(self):
         return f"Invoice number:  {self.invoice.number}, position title: {self.title}"
+
+
  
